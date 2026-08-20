4facb0a9df71b6bcb4f3d8f0b2fd70c6af26ce51
Bulić Amra <bulicamra00@gmail.com>
2026-08-21 00:09:35 +0200

    Fix axios base URL for Docker containers

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+
 const api = axios.create({
-  baseURL: 'http://localhost:5110/api/v1',
+  baseURL: 'http://localhost:8080/api/v1',
 });
 
 api.interceptors.request.use((config) => {
